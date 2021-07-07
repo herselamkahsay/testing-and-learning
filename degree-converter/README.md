4a4814cef87aa17d247e428102446e34a250a95f
herselamkahsay <herselam.kahsay@powercoders.org>
2021-07-06 23:13:39 -0900

    Right-to-left conversion

--- degree-converter/README.md
+++ degree-converter/README.md
@@ -1,10 +1,10 @@
-# testing-and-learning
-## DEGREE CONVERTER
+# DEGREE CONVERTER
+[see the result](https://herselamkahsay.github.io/testing-and-learning/degree-converter/index.html)
 
 I made simple html file with two input and two select tag 
-# Two inputs
+## Two inputs
 one for input value and the second for result 
-# Two select Tag
+## Two select Tag
 used to select between temperature types
 Celcius, fahrenheit kelvin
 ## some css style and media querries
--- degree-converter/app.js
+++ degree-converter/app.js
@@ -5,47 +5,47 @@
   
   convertToF(30);*/
 
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-const selectedInput = document.getElementById("selectedInput");
-const selectedOutput = document.getElementById("selectedOutput");
-var selectedInputValue, selectedOutputValue;
+const inputLeft = document.getElementById("input");
+const inputRight = document.getElementById("output");
+const selectLeft = document.getElementById("selectedInput");
+const selectRight = document.getElementById("selectedOutput");
+setupHandlers(inputLeft, selectLeft, inputRight, selectRight);
+setupHandlers(inputRight, selectRight, inputLeft, selectLeft);
 
-input.addEventListener("keyup", myResult);
-selectedInput.addEventListener("change", myResult);
-selectedOutput.addEventListener("change", myResult);
-selectedInputValue = selectedInput.value;
-selectedOutputValue = selectedOutput.value;
-function myResult() {
-  //output.value = input.value;
-  selectedInputValue = selectedInput.value;
-  selectedOutputValue = selectedOutput.value;
-  if (selectedInputValue === "celsius" && selectedOutputValue === "fahrenheit") {
-    output.value = (Number(input.value) * 1.8) + 32;
-  }
-  else if (selectedInputValue === "celsius" && selectedOutputValue === "Kelvin") {
-    output.value = Number(input.value) + 273.15;
-  }
-  else if (selectedInputValue === "celsius" && selectedOutputValue === "celsius") {
-    output.value = Number(input.value);
-  }
-  if (selectedInputValue === "fahrenheit" && selectedOutputValue === "celsius") {
-    output.value = (Number(input.value) - 32) / 1.8;
-  }
-  else if (selectedInputValue === "fahrenheit" && selectedOutputValue === "Kelvin") {
-    output.value = (Number(input.value) - 32) / 1.8 + 273.15;
-  }
-  else if (selectedInputValue === "fahrenheit" && selectedOutputValue === "fahrenheit") {
-    output.value = Number(input.value);
-  }
-  if (selectedInputValue === "Kelvin" && selectedOutputValue === "celsius") {
-    output.value = (Number(input.value) - 32) / 1.8;
-  }
-  else if (selectedInputValue === "Kelvin" && selectedOutputValue === "fahrenheit") {
-    output.value = (Number(input.value) - 32) / 1.8 + 273.15;
-  }
-  else if (selectedInputValue === "Kelvin" && selectedOutputValue === "Kelvin") {
-    output.value = Number(input.value);
+function setupHandlers(input, selectedInput, output, selectedOutput) {
+  input.addEventListener("keyup", myResult);
+  selectedInput.addEventListener("change", myResult);
+  function myResult() {
+    //output.value = input.value;
+    const selectedInputValue = selectedInput.value;
+    const selectedOutputValue = selectedOutput.value;
+    if (selectedInputValue === "celsius" && selectedOutputValue === "fahrenheit") {
+      output.value = (Number(input.value) * 1.8) + 32;
+    }
+    else if (selectedInputValue === "celsius" && selectedOutputValue === "Kelvin") {
+      output.value = Number(input.value) + 273.15;
+    }
+    else if (selectedInputValue === "celsius" && selectedOutputValue === "celsius") {
+      output.value = Number(input.value);
+    }
+    if (selectedInputValue === "fahrenheit" && selectedOutputValue === "celsius") {
+      output.value = (Number(input.value) - 32) / 1.8;
+    }
+    else if (selectedInputValue === "fahrenheit" && selectedOutputValue === "Kelvin") {
+      output.value = (Number(input.value) - 32) / 1.8 + 273.15;
+    }
+    else if (selectedInputValue === "fahrenheit" && selectedOutputValue === "fahrenheit") {
+      output.value = Number(input.value);
+    }
+    if (selectedInputValue === "Kelvin" && selectedOutputValue === "celsius") {
+      output.value = (Number(input.value) - 32) / 1.8;
+    }
+    else if (selectedInputValue === "Kelvin" && selectedOutputValue === "fahrenheit") {
+      output.value = (Number(input.value) - 32) / 1.8 + 273.15;
+    }
+    else if (selectedInputValue === "Kelvin" && selectedOutputValue === "Kelvin") {
+      output.value = Number(input.value);
+    }
   }
 }
 
