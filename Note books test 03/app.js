249bcc17b4d9d9302b641178b2ef73230a0562c8
herselamkahsay <herselam.kahsay@powercoders.org>
2021-07-06 20:34:07 -0900

    create function to open and close new note

--- Note books test 03/app.js
+++ Note books test 03/app.js
@@ -1,30 +1,31 @@
 var container2 = document.getElementsByClassName("container2")[0];
 var container3 = document.getElementsByClassName("container3")[0];
-var save = document.getElementsByName("add-icon");
-var xIcon = document.getElementsByName("x-icon");
+var save = document.getElementById("add-icon")
+var xIcon = document.getElementById("x-icon");
 var i = 0;
 
 //event listener
-xIcon.addEventListener("click", function () {
-    typeNote();
-
-})
-//remove when we click x-icon and open new note when we click create notes
-
 
 save.addEventListener("click", function () {
     makeNote();
 })
+
+xIcon.addEventListener("click", function () {
+    openNewNote();
+
+})
+//remove when we click x-icon and open new note when we click create notes
+
 //call the two function
 function makeNote() {
 
 }
-function typeNote() {
+function openNewNote() {
     if (container3.style.display == "none") {
-        container3.style.display == "block";
+        container3.style.display = "block";
     }
     else {
-        container3.style.display == "none";
+        container3.style.display = "none";
     }
-    ;
+
 }
