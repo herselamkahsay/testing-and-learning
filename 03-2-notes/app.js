177d96cf474c403a5ca65d2b15457a4c4680e72b
herselamkahsay <herselam.kahsay@powercoders.org>
2021-08-26 13:32:15 -0900

    Hide single note and visible notelist and vs Add span with className title and delete for each li Add event listener for each list to open into single note Add event listener to class name delete to remove each list Add event listener to addnote btn wich shows single note Fill id ,title and content Add init to showAll btn

--- 03-2-notes/app.js
+++ 03-2-notes/app.js
@@ -1,49 +1,74 @@
 const notes = [
-    { id: 1, title: "My first note", content: "First note content" },
-    { id: 2, title: "My second note", content: "Second note content" },
-    { id: 3, title: "My third note", content: "Third note content" },
+    { id: 61, title: "My first note", content: "First note content" },
+    { id: 62, title: "My second note", content: "Second note content" },
+    { id: 63, title: "My third note", content: "Third note content" },
 ]
 //console.log(notes[i].title);
 
 var noteList = document.getElementById("note-list");
 
 //console.log(noteList)
-//showNotesList();
+showNotesList();
 function showNotesList() {
+    const singleNote = document.getElementById("singleNote");
+    singleNote.style.display = "none";
+    const noteList = document.getElementById("note-list");
+    noteList.style.display = "block";
+
     // Create the list element:
     noteList.innerHTML = "";
     var list = document.createElement('ul');
 
-    for (var i = 0; i < notes.length; i++) {
+    for (let i = 0; i < notes.length; i++) {
 
         var note = document.createElement('li');
-        // Set its contents
-        note.appendChild(document.createTextNode(notes[i].title));
 
         // Add it to the list:
         list.appendChild(note);
-        //add delete button for each list
-        var deleteBtn = document.createElement('button');
-        deleteBtn.innerText = "delete"
-        list.appendChild(deleteBtn);
+        //create span for each li
+        let span = document.createElement('span');
+        note.appendChild(span);
+        span.classList.add('title');
+        // Set its contents
+        span.appendChild(document.createTextNode(notes[i].title));
+        span.addEventListener('click', (e) => {
+            e.preventDefault();
+            showSingleNote(notes[i].id);
 
-        deleteBtn.addEventListener('click', event => list.removeChild(note));
+        });
+        //add delete button for each list
+        var deleteBtn = document.createElement('span');
+        deleteBtn.innerText = "delete"
+        deleteBtn.classList.add('delete');
+        note.appendChild(deleteBtn);
+        list.addEventListener('click', function (e) {
+            if (e.target.classList == 'delete') {
+                const li = e.target.parentElement;
+                list.removeChild(li);
+            }
+        });
+
+        //add delete button for each list
+        /* var deleteBtn = document.createElement('button');
+         deleteBtn.innerText = "delete"
+         deleteBtn.classList.add('delete');
+         note.appendChild(deleteBtn);
+         list.addEventListener('click', function (e) {
+             if (e.target.className == 'delete') {
+                 list.removeChild(deleteBtn);
+             }
+         });*/
+
+
+
         //noteList.appendChild(deleteBtn);
 
         // Add class name
-        note.classList.add('title');
-        deleteBtn.classList.add('delete');
+        //note.classList.add('title');
 
     }
     //noteList.appendChild(list);
     noteList.appendChild(list);
-    noteList.addEventListener('click', (e) => {
-        e.preventDefault();
-        e.target = showSingleNote();
-
-    });
-
-
 
 
     // add button
@@ -54,45 +79,31 @@ function showNotesList() {
     // add class name in the button tag
     btn.type = "button";
     btn.classList.add('btn', 'btn-success');
-    btn.addEventListener('click', function addNewNote() {
 
-    });
-
+    //btn.addEventListener('click', showSingleNote(id));
 
 }
-
-
-//console.log(singleNote);
-showSingleNote();
 
 
 function showSingleNote(id) {
-    var singleNote1 = document.getElementById("singleNote");
-    singleNote1.style.display = "inline-block";
+    const singleNote1 = document.getElementById("singleNote");
+    singleNote1.style.display = "block";
+    const noteList = document.getElementById("note-list");
+    noteList.style.display = "none";
+
+    let note;
     for (let i = 0; i < notes.length; i++) {
-        id = notes[i];
+        if (id == notes[i].id) {
+            note = notes[i];
+        }
     }
-    //console.log(id);
 
-
-    var titles = document.getElementById("input-title").value;
-    //filling input
-    for (var i = 0; i < titles.length; i++) {
-        notes.title = titles[i].value;
-        console.log(titles);
-
-    }
-    //filling textarea
-    var contents = document.getElementById("note-text").value;
-
-    for (var i = 0; i < contents.length; i++) {
-        notes.content = contents[i].value;
-        console.log(contents);
-
-    }
+    document.getElementById("input-title").value = note.title;
+    document.getElementById("note-text").value = note.content;
 }
-function showAll() {
-    var showBtn = document.getElementById("add-icon");
+
+function initShowAll() {
+    var showBtn = document.getElementById("show-all");
     //console.log(showBtn);
     showBtn.addEventListener('click', (e) => {
         e.preventDefault();
@@ -100,4 +111,4 @@ function showAll() {
     });
 
 }
-showAll();
+initShowAll();
